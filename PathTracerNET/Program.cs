1d25764e46d393604ef6cf1462af3b08536d6fac
Shylie <fuzzcat927@gmail.com>
2019-09-01 08:37:42 -0400

    minor fixes with demo scene

--- PathTracerNET/Program.cs
+++ PathTracerNET/Program.cs
@@ -4,7 +4,7 @@ namespace PathTracerNET
 {
 	internal class Program
 	{
-		private const int WIDTH = 5120, HEIGHT = 2560, SAMPLES = 10;
+		private const int WIDTH = 1024, HEIGHT = 512, SAMPLES = 250;
 
 		private static void Main(string[] args)
 		{
@@ -12,7 +12,7 @@ namespace PathTracerNET
 			PTObject glass = PTObject.Dieletric(1f, 1f, 1f, 1.54f);
 			PTObject blueMatte = PTObject.Lambertian(0.4f, 0.7f, 0.9f);
 
-			PTObject light = PTObject.Sphere(new Vec3(0.4f, 3f, -0.4f), 2.4f, lightMaterial);
+			PTObject light = PTObject.Sphere(new Vec3(0.4f, 3f, -0.4f), 1.8f, lightMaterial);
 			PTObject sphere = PTObject.Sphere(new Vec3(0f, -100.1f, 0f), 100f, blueMatte);
 			PTObject cube = PTObject.RectangularPrism(-1f, 1f, 0f, 0.25f, -1f, 1f, glass);
 
@@ -20,7 +20,7 @@ namespace PathTracerNET
 
 			string fname = (args.Length == 1) ? args[0] : "test";
 
-			PTObject.RenderSceneChunked(WIDTH, HEIGHT, SAMPLES, fname, new Vec3(2f, 1f, -2f) - new Vec3(0f, 0.2f, 0f), new Vec3(-2f, 0f, 2f), new Vec3(0f, 1f, 0f), (float)Math.PI / 3.4f, (float)WIDTH / HEIGHT, scene);
+			PTObject.RenderSceneChunked(WIDTH, HEIGHT, SAMPLES, fname, new Vec3(2f, 0.6f, -2f), new Vec3(-2f, 0f, 2f), new Vec3(0f, 1f, 0f), (float)Math.PI / 3.4f, (float)WIDTH / HEIGHT, scene);
 
 			scene.Destroy();
 		}
